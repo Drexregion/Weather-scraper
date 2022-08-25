009247d2036f86e73db91a4546156397b41f506c
Drexregion <96681590+Drexregion@users.noreply.github.com>
2022-08-25 11:50:02 +0100

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,53 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-stocks = []
-page = requests.get("https://www.jumia.com.ng/macbooks/")
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=34.099695000000054&lon=-118.33539999999999#.XuijGEVKjIU")
 
 soup = BeautifulSoup(page.content, "html.parser")
-week = soup.find(class_ ='-paxs row _no-g _4cl-3cm-shs')
+week = soup.find(id ='seven-day-forecast-body')
+items =week.find_all(class_ ='tombstone-container')
 # print(week)
-items =week.find_all(class_ ='name')
-items_2 = week.find_all(class_="prc")
-# print(items)
-for i in range(20):
-	stocks.append(items[i].get_text() + "  : " + items_2[i].get_text())
-print(stocks)
+print(items)
 
-# # print(week)\
-# print(items)
-
-# # print(items[0])
+# print(items[0])
 
 
-# # printt(items[0].find(class_ = "period-name"))
+# printt(items[0].find(class_ = "period-name"))
 
 
-# print(items[0].find(class_ = "period-name")).get_text()
+print(items[0].find(class_ = "period-name")).get_text()
 
 
-# period_names = [item.find(class_ ="period-name").get_text() for item in items]
-# # print(period_names)
+period_names = [item.find(class_ ="period-name").get_text() for item in items]
+# print(period_names)
 
-# short_description = [item.find(class_ ="short-desc").get_text() for item in items]
-# # print(short_description)
+short_description = [item.find(class_ ="short-desc").get_text() for item in items]
+# print(short_description)
 
-# temp = [item.find(class_ ="temp").get_text() for item in items]
-# # stringed_temp = str(temp)
-# # print(stringed_temp)
+temp = [item.find(class_ ="temp").get_text() for item in items]
+# stringed_temp = str(temp)
+# print(stringed_temp)
 
 
 
-# import pandas as pd
+import pandas as pd
 
 
 
-# wethear_analysis = pd.DataFrame( {
-# 	"period":period_names,
-# 	"temperature":temp,
-# 	"description":short_description})
-# # print(wethear_analysis)
+wethear_analysis = pd.DataFrame( {
+	"period":period_names,
+	"temperature":temp,
+	"description":short_description})
+# print(wethear_analysis)
 
 
-# wethear_analysis.to_csv("weather.csv")
-# # print(wethear_analysis.to_csv("weather.csv"))
-# None
+wethear_analysis.to_csv("weather.csv")
+# print(wethear_analysis.to_csv("weather.csv"))
+None
